b1e9772315b015092e5998cef54cbb8cbfc6ab15
Italobgr <italobrunobgr@gmail.com>
2025-04-20 20:14:59 -0300

    feat: adding endpoint to register an user

---
+++ src/Shared/MyRecipeBook.Comunication/Responses/ResponseRegisterUserJson .cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecipeBook.Comunication.Responses
+{
+    public class ResponseRegisterUserJson
+    {
+        public string Name { get; set; } = string.Empty;
+
+    }
+}
