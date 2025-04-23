5c3388bd242c8e6d938648cbd97dd954c32a1770
Italobgr <italobrunobgr@gmail.com>
2025-04-22 21:25:27 -0300

    feat: new validation user with FluentValidation

---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using MyRecipeBook.Comunication.Requests;
+
+namespace MyRecipeBook.Application.UseCases.User.Register
+{
+    public class RegisterUserValidator : AbstractValidator<RequestRegisterUsersJson>
+    {
+
+        public RegisterUserValidator()
+        {
+
+            RuleFor(user => user.Name).NotEmpty();
+            RuleFor(user => user.Email).NotEmpty();
+            RuleFor(user => user.Email).EmailAddress();
+            RuleFor(user => user.Password.Length).GreaterThan(6);
+
+        }
+
+    }
+}
