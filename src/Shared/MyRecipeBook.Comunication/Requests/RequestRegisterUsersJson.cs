f050249e8e02be99e3ebce339803425082460df9
Italo Bruno <78346541+Italobgr@users.noreply.github.com>
2025-04-25 13:21:22 -0300

    Update RequestRegisterUsersJson.cs

--- src/Shared/MyRecipeBook.Comunication/Requests/RequestRegisterUsersJson.cs
+++ src/Shared/MyRecipeBook.Comunication/Requests/RequestRegisterUsersJson.cs
@@ -2,6 +2,7 @@
 {
     public class RequestRegisterUsersJson
     {
+        /**/
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
