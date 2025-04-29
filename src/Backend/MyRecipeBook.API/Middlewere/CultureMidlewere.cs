737184ccf12e66e24d626dd2e4e7451ea3e67446
Italobgr <italobrunobgr@gmail.com>
2025-04-28 21:32:36 -0300

    refact: finish new cultures events to exeption

--- src/Backend/MyRecipeBook.API/Middlewere/CultureMidlewere.cs
+++ src/Backend/MyRecipeBook.API/Middlewere/CultureMidlewere.cs
@@ -5,13 +5,27 @@ namespace MyRecipeBook.API.Middlewere
     public class CultureMidlewere
     {
 
+        private readonly RequestDelegate _next;
+
+        public CultureMidlewere(RequestDelegate next) 
+        {
+            _next = next;
+        }
+        
+
+
         public async Task Invoke(HttpContext context) 
         {
             var requestCulture = context.Request.Headers.AcceptLanguage.FirstOrDefault();//busca do herder
 
-            CultureInfo.CurrentCulture = new CultureInfo(requestCulture);//manda trocar
+            var culture = new CultureInfo(requestCulture);
+
+            CultureInfo.CurrentCulture = culture;//manda trocar
+            CultureInfo.CurrentUICulture = culture;
 
             //add UI
+
+            await _next(context);
 
 
         }
