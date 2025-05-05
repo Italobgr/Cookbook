b147dee6cd1c2c0e6d59f23e9c46ce9ac015b6e6
Italo Brunno <italo.leite@mgs.srv.br>
2025-05-05 16:59:42 -0300

    feat: finish a fist step of excepition validation

---
+++ src/Backend/MyRecipeBook.API/Filters/ExceptionFilter.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MyRecipeBook.Comunication.Responses;
+using MyRecipeBook.Exepition;
+using MyRecipeBook.Exepition.ExeptionsBase;
+using System.Net;
+
+namespace MyRecipeBook.API.Filters
+{
+    public class ExceptionFilter : IExceptionFilter //identifica a classa como  um filtro de excepitions 
+    {
+        public void OnException(ExceptionContext context)
+        {
+
+            if (context.Exception is MyRecipeBookExeption) //se o contexto da exeção for o mesmo do my
+                HandlerProjectException(context);            
+            else
+                throwUnknowExcepion(context);
+
+
+
+
+        }
+
+
+
+        private void HandlerProjectException(ExceptionContext context)
+        {
+
+            if (context.Exception is ErrorOnValidatoonExeption)
+            {
+
+                var excepition = context.Exception as ErrorOnValidatoonExeption;
+
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                context.Result = new BadRequestObjectResult(new ResponseErrorJson(excepition.ErrorsMessages)); //result devolve para o body da minha resposta
+            }
+
+        }
+
+
+        private void throwUnknowExcepion(ExceptionContext context)
+        {
+
+            if (context.Exception is ErrorOnValidatoonExeption)
+            {
+
+                /*FALTA CRIAR O RESTO DOS UNKNOW_ERROR ITALIANO E PORTUGUES  */
+
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;//internal server erro usa o obj result
+                context.Result = new ObjectResult(new ResponseErrorJson(ResourceMensagesExeption.UNKNOW_ERROR)); //result devolve para o body da minha resposta
+            }
+
+        }
+
+
+    }
+}
--- src/Backend/MyRecipeBook.API/Program.cs
+++ src/Backend/MyRecipeBook.API/Program.cs
@@ -1,3 +1,4 @@
+using MyRecipeBook.API.Filters;
 using MyRecipeBook.API.Middlewere;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+//faz a api escutar qq exception gerada e executar o filtro
+builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter))); //typeoff se molda para um typo expecifico
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
